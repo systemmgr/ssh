d066e4c421212288f43519f02ec9c0476462d9d1
Jason <jason@casjay.net>
2021-04-08 18:10:17 -0400

    🦈🏠❗ Well I did things - Hopefully nothing breaks ❗🏠🦈

--- install.sh
+++ install.sh
@@ -48,11 +48,15 @@ scripts_check
 # Defaults
 APPNAME="${APPNAME:-ssh}"
 APPDIR="/usr/local/etc/$APPNAME"
-INSTDIR="$SYSSHARE/CasjaysDev/$SCRIPTS_PREFIX/$APPNAME"
-REPO_BRANCH=${GIT_REPO_BRANCH:-master}
-REPO="https://github.com/systemmgr/$APPNAME"
-REPORAW="$REPO/raw/$GIT_REPO_BRANCH"
+INSTDIR="$SYSSHARE/CasjaysDev/systemmgr/$APPNAME"
+REPO_BRANCH="${GIT_REPO_BRANCH:-master}"
+REPO="${SYSTEMMGRREPO:-https://github.com/systemmgr}/$APPNAME"
+REPORAW="$REPO/raw/$REPO_BRANCH"
 APPVERSION="$(__appversion "$REPORAW/version.txt")"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Setup plugins
+PLUGNAMES=""
+PLUGDIR="${SHARE:-$HOME/.local/share}/$APPNAME"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Require a version higher than
 systemmgr_req_version "$APPVERSION"
@@ -139,6 +143,10 @@ execute "run_postinst" "Running post install scripts"
 # create version file
 systemmgr_install_version
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# exit
+# run exit function
 run_exit
-# end
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# End application
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# lets exit with code
+exit ${exitCode:-$?}
